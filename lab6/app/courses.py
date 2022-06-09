ae43dea9d6590329ce7560d7ebdafd5fc9a5b323
mksmp <panin.ma2013u@mail.ru>
2022-06-09 12:35:37 +0300

    Lab 6 is done full

--- lab6/app/courses.py
+++ lab6/app/courses.py
@@ -1,9 +1,9 @@
 from crypt import methods
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 from app import db
 from models import Course, Category, Review, User
-from tools import CoursesFilter, ImageSaver
+from tools import CoursesFilter, ImageSaver, ReviewsFilter
 
 bp = Blueprint('courses', __name__, url_prefix='/courses')
 
@@ -11,22 +11,35 @@ PER_PAGE = 3
 
 COMMENT_PAGE = 5
 
+PER_PAGE_COMMENTS = 10
+
 COURSE_PARAMS = ['author_id', 'name', 'category_id', 'short_desc', 'full_desc']
 
 COMMENT_PARAMS = ['rating', 'text', 'course_id', 'user_id']
 
+
 def params():
     return { p: request.form.get(p) for p in COURSE_PARAMS }
 
+
 def comment_params():
     return { p: request.form.get(p) for p in COMMENT_PARAMS }
-    
 
+    
 def search_params():
     return {
         'name': request.args.get('name'),
         'category_ids': request.args.getlist('category_ids')
     }
+
+
+def search_params_comm(course_id):
+    return {
+        'name': request.args.get('name'),
+        'category_ids': request.args.getlist('category_ids'),
+        'course_id': course_id
+    }
+
 
 @bp.route('/')
 def index():
@@ -38,6 +51,7 @@ def index():
     return render_template('courses/index.html', courses=courses, categories=categories, pagination=pagination, search_params=search_params())
 
 
+@login_required
 @bp.route('/new')
 def new():
     categories = Category.query.all()
@@ -45,6 +59,7 @@ def new():
     return render_template('courses/new.html', categories=categories, users=users)
 
 
+@login_required
 @bp.route('/create', methods=['POST'])
 def create():
 
@@ -63,11 +78,14 @@ def create():
 def show(course_id):
     courses = Course.query.get(course_id)
     reviews = Review.query.filter_by(course_id=course_id).limit(COMMENT_PAGE)
-    user_review = Review.query.filter_by(course_id=course_id, user_id=current_user.id).first()
+    user_review = None
+    if current_user.is_authenticated is True:
+        user_review = Review.query.filter_by(course_id=course_id, user_id=current_user.id).first()
     users = User.query.all()
 
     return render_template('courses/show.html', course=courses, review=reviews, users=users, user_review=user_review)
 
+@login_required
 @bp.route('/<int:course_id>', methods=['POST'])
 def send_comment(course_id):
     reviews = Review(**comment_params())
@@ -79,23 +97,38 @@ def send_comment(course_id):
     flash('Комментарий был успешно добавлен!', 'success')
     return redirect(url_for('courses.show', course_id=courses.id))
 
+
 @bp.route('/<int:course_id>/reviews')
 def reviews(course_id):
-    reviews = Review.query.filter_by(course_id=course_id).all()
+    page = request.args.get('page', 1, type=int)
+    # reviews = Review.query.filter_by(course_id=course_id).all()
+    reviews = ReviewsFilter(**search_params()).perform()
     courses = Course.query.filter_by(id=course_id).first()
-    return render_template('courses/reviews.html', reviews=reviews, courses=courses)
+    pagination = reviews.paginate(page, PER_PAGE_COMMENTS)
+    reviews = pagination.items
+    return render_template('courses/reviews.html', reviews=reviews, courses=courses, pagination=pagination, search_params=search_params_comm(course_id))
+    # return render_template('courses/reviews.html', reviews=reviews, courses=courses)
+
 
 @bp.route('/<int:course_id>/reviews', methods=['POST'])
 def reviews_sort(course_id):
+    page = request.args.get('page', 1, type=int)
     reviews = Review.query.filter_by(course_id=course_id).all()
-    if request.form.get('sort-date') == 'new' and request.form.get('sort-rating') == 'good':
-        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.created_at.asc(), Review.rating.desc()).all()
-    if request.form.get('sort-date') == 'new' and request.form.get('sort-rating') == 'bad':
-        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.created_at.asc(), Review.rating.asc()).all()
-    if request.form.get('sort-date') == 'old' and request.form.get('sort-rating') == 'good':
-        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.created_at.desc(), Review.rating.desc()).all()
-    if request.form.get('sort-date') == 'new' and request.form.get('sort-rating') == 'bad':
-        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.created_at.desc(), Review.rating.asc()).all()
-    req_form = [request.form.get('sort-date'), request.form.get('sort-rating')]
+    
+    # reviews = CoursesFilter(**search_params()).perform()
+
+    if request.form.get('sort') == 'new':
+        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.created_at.desc()).all()
+    if request.form.get('sort') == 'old':
+        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.created_at.asc()).all()
+    if request.form.get('sort') == 'good':
+        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.rating.desc()).all()
+    if request.form.get('sort') == 'bad':
+        reviews = Review.query.filter_by(course_id=course_id).order_by(Review.rating.asc()).all()
+    req_form = request.form.get('sort')
     courses = Course.query.filter_by(id=course_id).first()
+    # сделать один селектор и сортировать по нему!!!
+    # pagination = courses.paginate(page, PER_PAGE_COMMENTS)
+    # courses = pagination.items
     return render_template('courses/reviews.html', reviews=reviews, courses=courses, req_form=req_form)
+    # return render_template('courses/reviews.html', reviews=reviews, courses=courses, req_form=req_form, pagination=pagination, search_params=search_params())
--- lab6/app/tools.py
+++ lab6/app/tools.py
@@ -1,6 +1,6 @@
 import hashlib
 import uuid
-from models import Course, Image
+from models import Course, Image, Review
 import os
 from werkzeug.utils import secure_filename
 from app import db, app
@@ -48,3 +48,25 @@ class ImageSaver:
         self.md5_hash = hashlib.md5(self.file.read()).hexdigest()
         self.file.seek(0)
         return Image.query.filter(Image.md5_hash == self.md5_hash).first()
+
+
+class ReviewsFilter:
+    def __init__(self, name, category_ids):
+        self.name = name
+        self.category_ids = category_ids
+        self.query = Review.query
+
+    def perform(self):
+        self.__filter_by_name()
+        self.__filter_by_category_ids()
+        return self.query.order_by(Review.created_at.desc())
+
+    def __filter_by_name(self):
+        if self.name:
+            self.query = self.query.filter(
+                Review.name.ilike('%' + self.name + '%'))
+
+    def __filter_by_category_ids(self):
+        if self.category_ids:
+            self.query = self.query.filter(
+                Review.category_id.in_(self.category_ids))
